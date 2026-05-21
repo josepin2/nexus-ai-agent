b4c38542c55013a5e8ada98125538ae7a08e11a7
JosePin2 <josepin2@users.noreply.github.com>
2026-05-21 21:05:06 +0200

    Añadir archivo de configuración config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+"""
+Configuración del Chatbot
+"""
+
+# Configuración de Ollama
+OLLAMA_BASE_URL = "http://localhost:11434"
+
+# Copyright 2026 José Milán Carrasco
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+
+"""
+Configuración del servidor
+"""
+HOST = "127.0.0.1"
+PORT = 8000
+
+# Configuración de la interfaz
+INTERFACE_PORT = 3000
